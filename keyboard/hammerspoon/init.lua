1c368aa89ae15acd0a4d3a97a2953993b486dbbf
Tristan Dunn <tristanzdunn@gmail.com>
2020-09-19 20:56:13 -0500

    Remove the notification for reloading Hammerspoon configuration.

--- keyboard/hammerspoon/init.lua
+++ keyboard/hammerspoon/init.lua
@@ -7,5 +7,4 @@ require("keyboard.windows")
 -- Bind Control+` to reload the Hammerspoon configuration.
 hs.hotkey.bind({"ctrl"}, "`", nil, function()
   hs.reload()
-  hs.notify.new({title="Hammerspoon", informativeText="Configuration reloaded."}):send()
 end)
