0fab6b8ef2f16440ae5f2d69dd865b7dcbddeeaa
Tristan Dunn <tristanzdunn@gmail.com>
2020-11-16 22:51:21 -0600

    Fix keyboard applications watcher by making it a global.

--- keyboard/hammerspoon/chrome.lua
+++ keyboard/hammerspoon/chrome.lua
@@ -1,3 +1,5 @@
+local chromeWatcher = nil
+
 function onApplicationEvent(name, event, application)
   -- Ignore applications that aren't Chrome.
   if name ~= "Google Chrome" then
@@ -9,40 +11,39 @@ function onApplicationEvent(name, event, application)
     watchApplication(application)
   elseif event == hs.application.watcher.terminated then
     -- Stop watching Chrome when terminated.
-    if applicationWatcher then
-      applicationWatcher:stop()
-      applicationWatcher = nil
+    if chromeWatcher then
+      chromeWatcher:stop()
+      chromeWatcher = nil
     end
   end
 end
 
 function watchApplication(application)
   -- Create a watcher for the Chrome application.
-  applicationWatcher = application:newWatcher(function(window)
-    -- Sometimes window appears to be nil, but not sure why.
-    if window == nil then
+  chromeWatcher = application:newWatcher(function(window)
+    -- Sometimes window appears to be missing.
+    if not window then
       return
     end
 
     local title = window:title()
 
     -- Only reposition browser windows.
-    if title and string.match(title, "New Tab") or string.match(title, "Untitled") then
+    if string.match(title, "New Tab") or string.match(title, "Untitled") then
       -- Move the window to the default size and position.
       hs.window.default(window)
     end
   end)
 
   -- Start watching for windows being created.
-  applicationWatcher:start({hs.uielement.watcher.windowCreated})
+  chromeWatcher:start({hs.uielement.watcher.windowCreated})
 end
 
 -- Create an application watcher to watch for Chrome launching.
-local applicationWatcher  = nil
-local applicationsWatcher = hs.application.watcher.new(onApplicationEvent)
+applicationsWatcherForChrome = hs.application.watcher.new(onApplicationEvent)
 
 -- Start watching all applications.
-applicationsWatcher:start()
+applicationsWatcherForChrome:start()
 
 -- Attempt to find running Chrome application.
 local application = hs.appfinder.appFromName("Google Chrome")
