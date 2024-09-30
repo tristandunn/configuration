775e8288cadf9aff096d050f18ec2d9dd77b3bfd
Tristan Dunn <tristanzdunn@gmail.com>
2024-09-30 16:45:36 -0400

    Add shortcut to add new file.

--- nvim/plug-ins/gitsigns.lua
+++ nvim/plug-ins/gitsigns.lua
@@ -2,6 +2,7 @@ return {
   init = function()
     -- Shortcuts.
     vim.keymap.set("n", "<Leader>sb", "<ESC>:Gitsigns stage_buffer<CR>")
+    vim.keymap.set("n", "<Leader>sf", "<ESC>:!git add %:p<CR><CR>")
     vim.keymap.set("n", "<Leader>sh", "<ESC>:Gitsigns stage_hunk<CR>")
     vim.keymap.set("n", "<Leader>gdc", "<ESC>:ExecuteCommandInPane git\\ diff\\ --cached 1 1<CR>")
     vim.keymap.set("n", "<Leader>gdi", "<ESC>:ExecuteCommandInPane git\\ diff 1 1<CR>")
