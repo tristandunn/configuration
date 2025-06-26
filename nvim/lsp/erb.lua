8f36ff7cecf46c3984f0d43a3cb4fe69ee7354f2
Tristan Dunn <hello@tristandunn.com>
2025-06-25 23:17:26 -0400

    Remove Ruby from LSP configuration for ERB.

--- nvim/lsp/erb.lua
+++ nvim/lsp/erb.lua
@@ -1,6 +1,6 @@
 return {
   cmd          = { "herb-language-server", "--stdio" },
   capabilities = require("blink.cmp").get_lsp_capabilities(),
-  filetypes    = { "html", "ruby", "eruby" },
+  filetypes    = { "html", "eruby" },
   root_markers = { "Gemfile", ".git" }
 }
