41e008901fe26b059bd1d39535b146312e8c62f1
Tristan Dunn <hello@tristandunn.com>
2025-07-29 21:56:23 -0400

    Add YAML schemas for common Rails files.

--- nvim/lsp/yaml.lua
+++ nvim/lsp/yaml.lua
@@ -4,5 +4,20 @@ return {
   filetypes           = { "eruby.yaml", "yaml" },
   root_markers        = { "Gemfile", "package.json" },
   single_file_support = true,
-  settings            = { redhat = { telemetry = { enabled = false } } }
+  settings            = {
+    redhat = { telemetry = { enabled = false } },
+    yaml = {
+      schemas = {
+        ["https://www.rubyschema.org/i18n/locale.json"]     = "locale/*.yml",
+        ["https://www.rubyschema.org/kamal/deploy.json"]    = "deploy.yml",
+        ["https://www.rubyschema.org/rails/cable.json"]     = "cable.yml",
+        ["https://www.rubyschema.org/rails/cache.json"]     = "cache.yml",
+        ["https://www.rubyschema.org/rails/database.json"]  = "database.yml",
+        ["https://www.rubyschema.org/rails/queue.json"]     = "queue.yml",
+        ["https://www.rubyschema.org/rails/recurring.json"] = "recurring.yml",
+        ["https://www.rubyschema.org/rails/storage.json"]   = "storage.yml",
+        ["https://www.rubyschema.org/rubocop.json"]         = ".rubocop.yml"
+      }
+    }
+  }
 }
